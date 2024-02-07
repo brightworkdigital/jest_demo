1cc185060612c7b0e2bbfdbfdfb6bc41729140a2
Dave <davedykes@davedykes.com>
2024-02-07 08:56:21 +0000

    Changed check for OK response to use .ok rather than magic number 200.

--- src/api/rosterApi.js
+++ src/api/rosterApi.js
@@ -3,7 +3,7 @@ export function getNames() {
     const url = 'http://localhost:8080/names';
     return fetch(url)
     .then(response => {
-        if(response.status === 200) {
+        if(response.ok) {
             return response.json();
         } else {
             console.error(`Respnose error in getNames: ${response.status}`);
